4a3fcd20a37d2a99cbdd2a80bee346a7b4c4b1df
Namka <namsrai.khatanbaatar@gmail.com>
2023-05-29 14:49:53 +0800

    init empty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
+'use client';
 import React, { useEffect, useState } from "react";
 
 export default function Home() {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    import("../firebase").then((firebase) => {
-      const auth = firebase.getAuth();
-      auth.onAuthStateChanged((user) => {
-        setUser(user);
-        setLoading(false);
-      });
-    });
-  }, []);
-
-  const handleSignIn = async () => {
-    let provider;
-    let auth;
-    try {
-      const firebase = await import("../firebase");
-      provider = new firebase.GoogleAuthProvider();
-      auth = firebase.getAuth();
-      await firebase.signInWithPopup(auth, provider);
-    } catch (error) {
-      console.log(`Error ${error.code}: ${error.message}`);
-    }
-  };
-
+  const [loading, setLoading] = useState(false);
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -35,9 +12,14 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="center">
         <button onClick={handleSignIn}>
-          {user ? `Signed in as ${user.displayName}` : "Sign in with Google"}
+          {user ? `Signed in as ` : "Sign in with Google"}
         </button>
       </div>
     </main>
+
   );
+
+  async function handleSignIn() {
+    console.log("signing in");
+  }
 }
