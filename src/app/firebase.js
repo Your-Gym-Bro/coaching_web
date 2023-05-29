6b30dcc63b553ad00e880ba6934617e7f37952c5
Namka <namsrai.khatanbaatar@gmail.com>
2023-05-29 21:20:44 +0800

    firebase update

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -2,13 +2,16 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAZCCAc7TRAHGb49ral__4ysw4pRDaxPxQ",
-  authDomain: "your-auth-domain",
-  projectId: "labo001-77e8b",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "1:498577113087:android:111ca5a6a369f8bb97b1ca",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) {
