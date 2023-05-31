deab11dcd5c34037a1158097f603fe494bab1192
Namka <namsrai.khatanbaatar@gmail.com>
2023-06-01 02:34:24 +0800

    firebase keys from env file

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -3,12 +3,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAZCCAc7TRAHGb49ral__4ysw4pRDaxPxQ",
-  authDomain: "your-auth-domain",
-  projectId: "labo001-77e8b",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "1:498577113087:android:111ca5a6a369f8bb97b1ca",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJET_ID,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 if (!firebase.apps.length) {
